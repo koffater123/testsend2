a9a6e2f3a79bb932168a1da413b730ac63c73bdf
koffater123 <koffater123@hotmail.com>
2017-07-06 10:43:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
+			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
+			// Get text sent
+			$text = $event['replyToken'];
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
