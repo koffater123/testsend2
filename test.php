d01997d0cd8ef5c161314286834883b4ae068743
koffater123 <koffater123@hotmail.com>
2017-07-09 10:08:02 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,8 @@
 <?php
-		/*	$messages = [
+			$messages = [
 				  'type'=> 'template',
-				  'altText'=> 'this is a confirm template',
+				  'altText'=> 'this is a confirm template'
+				/*
 				  'template'=> {
 				      'type'=> 'confirm',
 				      'text'=> 'Are you sure?',
@@ -17,8 +18,8 @@
 					    'text'=> 'no'
 					  }
 				      ]
-				  }
+				  }*/
 
-					];*/
+					];
 
 echo "OK";
