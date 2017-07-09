7ab1063a85bbb8502e64c28f68ced5fa42ab1997
koffater123 <koffater123@hotmail.com>
2017-07-09 09:08:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,8 +20,24 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
+				'type'=> 'template',
+  				'altText'=> 'this is a confirm template',
+  				'template'=> {
+      				'type'=> 'confirm',
+			      	'text'=> 'Are you sure?',
+			      	'actions'=> [
+				  {
+				    'type'=> 'message',
+				    'label'=> 'Yes',
+				    'text'=> 'yes'
+				  },
+				  {
+				    'type'=>'message',
+				    'label'=> 'No',
+				    'text'=> 'no'
+				  }
+			      ]
+			  }
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
