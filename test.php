dc949080e8274e8d41377c76bf118eccfa641dc8
koffater123 <koffater123@hotmail.com>
2017-07-09 10:50:41 +0700

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,23 @@
 			$messages = array(
 				  'type'=> 'template',
 				  'altText'=> 'this is a confirm template'
-					);
+				'template'=>array (
+			      'type'=> 'confirm',
+			      'text'=> 'Are you sure?',
+			      'actions'=>array (
+				  (
+				    'type'=> 'message',
+				    'label'=> 'Yes',
+				    'text'=> 'yes'
+				  ),
+				  (
+				    'type'=> 'message',
+				    'label'=> 'No',
+				    'text'=> 'no'
+				  )
+			      )
+			  )
+								);
 $array = array(
     "foo" => "bar",
     42    => 24,
