fd8ef403e0501824d3e0bf901347f56e8aec191c
koffater123 <koffater123@hotmail.com>
2017-07-09 10:54:29 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 			$messages = array(
 				  'type'=> 'template',
-				  'altText'=> 'this is a confirm template',
+				  'altText'=> 'this is a confirm template'/*,
 				'template'=>array (
 			      'type'=> 'confirm',
 			      'text'=> 'Are you sure?',
@@ -17,7 +17,7 @@
 				    'text'=> 'no'
 				  )
 			      )
-			  )
+			  )*/
 								);
 $array = array(
     "foo" => "bar",
