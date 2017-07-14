de86dd8c9dbb8bde2a09c81cff98ef4d5f73c500
koffater123 <koffater123@hotmail.com>
2017-07-14 18:27:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = array(
 				'replyToken' => $replyToken,
-				'messages' => [$messages]
+				'messages' => [$messages],
 			);
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
